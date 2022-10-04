6b0fa2377ac0e2c62a46e58b8960d995d2acf5f1
pegaser <sperezgabriel@gmail.com>
2022-10-03 20:04:38 -0500

    Octubre 2022

---
+++ Homework/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnAswer_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtQuestion.Text))
+            {
+                MessageBox.Show("Pregunta algo antes.");
+            }
+            else
+            {
+                this.Cursor = Cursors.Wait;
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+                Random random = new Random();
+                txtAnswer.Text = random.Next(0, 2) == 0 ? "No" : "Si";
+                this.Cursor = null;
+            }
+        }
+    }
+}
